1bc46414da8f304e2316b07caa07b51ac3db968b
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2025-12-15 22:55:17 +0530

    Refactor Demo class to include new properties and methods

--- firstSpringApplication/src/main/java/kapilit/Demo.java
+++ firstSpringApplication/src/main/java/kapilit/Demo.java
@@ -1,32 +1,71 @@
 package kapilit;
 
+import java.util.List;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Demo {
 
-	private String value;
+	private int i;
+    private int j;
+    private List<Integer> numbers;
+    
+	/*public Demo(String value, int i, int j) {
+		super();
+		this.value = value;
+		this.i = i;
+		this.j = j;
+	} */
+    
+    
 
 	public Demo() {
 		super();
 	}
 
-	public Demo(String value) {
-		super();
-		this.value = value;
+	 public Demo(int i, int j, List<Integer> numbers) {
+	        this.i = i;
+	        this.j = j;
+	        this.numbers = numbers;
+	    }
+
+
+
+	public void show() {
+		System.out.println("hello");
+		System.out.println(i+j);
 	}
 
-	public String getValue() {
-		return value;
+	public int getI() {
+		return i;
 	}
 
-	public void setValue(String value) {
-		this.value = value;
+	public void setI(int i) {
+		this.i = i;
 	}
 
-	@Override
-	public String toString() {
-		return "Demo [value=" + value + "]";
+	public int getJ() {
+		return j;
 	}
 
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
+
+	public void setNumbers(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
 	
 	
-	
+	public void display() {
+		System.out.println("int , int , list");
+		
+		int sumOfEvenSquares = numbers.stream().filter(n -> n % 2 == 0).mapToInt(n -> n * n).sum();
+
+        System.out.println(sumOfEvenSquares);
+	}
 }
