ab55b3c6f43feb0dbc010497668b3f57df8d7bcf
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2025-12-18 21:54:51 +0530

    Add XML comments for Spring bean configuration
    Added XML configuration comments for Spring bean definition.

--- firstSpringApplication/src/main/java/kapilit/colleage.java
+++ firstSpringApplication/src/main/java/kapilit/colleage.java
@@ -20,5 +20,18 @@ public class colleage {
 	return "colleage [cid=" + cid + ", cName=" + cName + ", map=" + map + "]";
  }
  
- 
+ /*
+  * <bean id="clg" class="kapilit.colleage">
+
+<constructor-arg>
+            <map>
+                 <entry key="1" value="srm"/>
+                <entry key="2" value="bit"/>
+                <entry key="3" value="rcert"/>
+
+            </map>
+        </constructor-arg>
+
+</bean>
+  */
 }
