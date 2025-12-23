76fada709b0f90e6f98e583a39a761e446ec2d59
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2025-12-23 22:33:43 +0530

    Implement Spring context initialization in MainApp

--- AnnotationssDemo/src/main/java/kapilit/MainApp.java
+++ AnnotationssDemo/src/main/java/kapilit/MainApp.java
@@ -1,5 +1,13 @@
 package kapilit;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MainApp {
 
+    public static void main(String[] args) {
+    	 AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
 }
