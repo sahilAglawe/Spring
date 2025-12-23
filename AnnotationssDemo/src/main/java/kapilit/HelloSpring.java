a44d0a00115486a6d502888b36529544daaefcb5
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2025-12-23 22:31:29 +0530

    Add Spring component with message method

--- AnnotationssDemo/src/main/java/kapilit/HelloSpring.java
+++ AnnotationssDemo/src/main/java/kapilit/HelloSpring.java
@@ -1,5 +1,11 @@
 package kapilit;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloSpring {
 
+    public String getMessage() {
+        return "Hello, Spring Annotations!";
+    }
 }
