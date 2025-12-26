8ba4f0864c7a711c79ab4951462a219e863d66b4
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2025-12-26 12:15:46 +0530

    Add files via upload

---
+++ secondAnnotations/src/main/java/jpaDemo/AppConfig.java
@@ -0,0 +1,53 @@
+package jpaDemo;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+public class AppConfig {
+
+	 // ===================== DATASOURCE =====================
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/testdb");
+        ds.setUsername("root");
+        ds.setPassword("root");
+
+        return ds;
+    }
+
+    // ===================== ENTITY MANAGER FACTORY =====================
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean emf =
+                new LocalContainerEntityManagerFactoryBean();
+
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("jpademo.entity");
+
+        HibernateJpaVendorAdapter vendorAdapter =
+                new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
+
+        emf.setJpaVendorAdapter(vendorAdapter);
+
+        // Inline Hibernate properties (no separate method)
+        Properties props = new Properties();
+        props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        props.put("hibernate.hbm2ddl.auto", "update");
+
+        emf.setJpaProperties(props);
+
+        return emf;
+    }
+}
